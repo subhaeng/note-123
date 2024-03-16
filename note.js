2cdb0204a21f704775761f1d5b2cf88a3155203e
subhaeng <153849322+subhaeng@users.noreply.github.com>
2024-03-17 01:56:18 +0530

    Add files via upload

---
+++ note.js
@@ -0,0 +1,18 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "img/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+    }
+})
